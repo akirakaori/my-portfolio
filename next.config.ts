63c7db7f58888e23eed7d787c4f44dd2d3458cb3
Ashika Kambang <ashikakambang@gmail.com>
2026-06-17 17:22:40 +0545

    fixed the isseue that was casuing my file to not to deploy the file okay

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  distDir: ".next-build",
   devIndicators: false,
   turbopack: {
     root: path.join(__dirname),
